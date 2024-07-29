f8bff99ac3242dc02a8ba6bbd99c5b7a6eea4805
rlmgtr <rlmagtira@gmail.com>
2024-07-29 15:20:12 +0800

    update

---
+++ catapp/src/cat.api.js
@@ -0,0 +1,7 @@
+const BASE_URL = "https://api.thecatapi.com/v1";
+const API_KEY = "live_J9QkgshB2A5IXJFOZuLC7K6IskaQ4lYsPnuVtoFdmvj6zZsyrWlvCOYe5uwiMvW2";
+
+export function fetchCatBreeds () {};
+
+
+export function fetchCatbyBreed () {};
